522d6f26962617cf3f50d6c88e7a7e89e381f3ea
Kirill Kainov <kainov.dev@gmail.com>
2022-09-22 11:50:58 +0300

    add knapsack greedy method

---
+++ src/knapsack_greedy/KnapsackGreedy.java
@@ -0,0 +1,82 @@
+package knapsack_greedy;
+
+import java.util.Arrays;
+
+public class KnapsackGreedy {
+
+    public static int knapsackGreedy(int n, int wMax, int[] w, int[] c) {
+        System.out.println("Initial parameters:");
+        System.out.println("Items count: " + n);
+        System.out.println("W max = " + wMax);
+        System.out.println("Weights: " + Arrays.toString(w));
+        System.out.println("Costs: " + Arrays.toString(c));
+        System.out.println();
+
+        int sumW = 0;
+        int sumC = 0;
+        int[] order = naturalOrder(n);
+        int[] ans = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (c[i] < c[j]) {
+                    int tmp = c[i];
+                    c[i] = c[j];
+                    c[j] = tmp;
+
+                    tmp = w[i];
+                    w[i] = w[j];
+                    w[j] = tmp;
+
+                    tmp = order[i];
+                    order[i] = order[j];
+                    order[j] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Step " + (i + 1));
+            System.out.println("Current item: " + (order[i]+1));
+            System.out.println("w="+w[order[i]] + ", c=" + c[order[i]]);
+            if (sumW + w[i] <= wMax) {
+                System.out.println("TAKE");
+                sumW += w[i];
+                sumC += c[i];
+                ans[order[i]] = 1;
+            }
+            System.out.println("sumW=" + sumW + ", sumC=" + sumC);
+            System.out.println("*************************");
+        }
+
+        System.out.println("Found solution:");
+        System.out.println(Arrays.toString(ans));
+        return sumC;
+    }
+
+    private static void printInOrder(int[] ans, int[] order) {
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[order[i]] + " ");
+        }
+        System.out.println();
+    }
+
+    private static int[] naturalOrder(int n) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = i;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int wMax = 15;
+        int[] w = new int[]{5, 2, 6, 4, 7};
+        int[] c = new int[]{4, 2, 7, 5, 7};
+
+        int ans = knapsackGreedy(n, wMax, w, c);
+        System.out.println(ans);
+    }
+}
+
+
